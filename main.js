9d60a2fbb7f1b8ed5c990826c783f4d925c81e18
Student <student@RU-106.local>
2016-01-21 14:54:39 -0700

    last commit

--- main.js
+++ main.js
@@ -4,11 +4,48 @@ angular.module('module1', []);
 angular.module('module1').controller('modcontroller', ['$scope', function($scope) {
 
 
+	$scope.UserInfo = ['First name', 'Email address', 'City and State'];
 
-		$scope.userClick = function() {
+		$scope.nameClick = function() {
 
-			$scope.userHide = true;
-			$scope.inputHide = true;
+			$scope.nameHide = true;
+			$scope.nameInputHide = true;
+
+		}
+
+			$scope.descriptionClick = function() {
+
+			$scope.descriptionHide = true;
+			$scope.descriptionInputHide = true;
+
+		}
+
+			$scope.addressClick = function() {
+
+			$scope.addressHide = true;
+			$scope.addressInputHide = true;
+
+		}
+
+		$scope.blurClick = function(event) {
+
+			$scope.nameHide = false;
+			$scope.nameInputHide = false;
+			
+		}
+
+		$scope.blurClick1 = function(event) {
+
+			$scope.descriptionHide = false;
+			$scope.descriptionInputHide = false;
+			
+		}
+
+		$scope.blurClick2 = function(event) {
+
+			$scope.addressHide = false;
+			$scope.addressInputHide = false;
+			
 		}
 
 
